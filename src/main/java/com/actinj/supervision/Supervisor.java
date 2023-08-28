9d2a527ef2aac4049e8a23d68646207c122622da
Tijl Vercaemer <t7vykc55@duck.com>
2023-08-28 21:55:26 +0100

    Stop looping over children when we restart remaining or all children

--- src/main/java/com/actinj/supervision/Supervisor.java
+++ src/main/java/com/actinj/supervision/Supervisor.java
@@ -7,11 +7,9 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Supplier;
@@ -137,18 +135,24 @@ public class Supervisor implements Supplier<ChildSpec>, Runnable {
     }
 
     private void checkChildren(AtomicReference<List<Instant>> restarts) {
-        for (final ThreadConfig child : children) {
-            if (!child.thread().isAlive()) {
-                switch (child.spec().restart()) {
-                case Restart.TRANSIENT -> transientChildStopped(child, restarts);
-                case Restart.TEMPORARY -> logger.info("Temporary thread {} stopped", child.spec.id());
-                case Restart.PERMANENT -> permanentChildStopped(child, restarts);
+        try {
+            for (final ThreadConfig child : children) {
+                if (!child.thread().isAlive()) {
+                    switch (child.spec().restart()) {
+                    case Restart.TRANSIENT -> transientChildStopped(child, restarts);
+                    case Restart.TEMPORARY -> logger.info("Temporary thread {} stopped", child.spec.id());
+                    case Restart.PERMANENT -> permanentChildStopped(child, restarts);
+                    }
                 }
             }
+        } catch (StopChildrenCheckLoop e) {
+            // We're done looping. A stopped thread caused all remaining threads to restart, so no more need to check
+            // them
         }
     }
 
-    private void transientChildStopped(final ThreadConfig child, final AtomicReference<List<Instant>> restarts) {
+    private void transientChildStopped(final ThreadConfig child, final AtomicReference<List<Instant>> restarts)
+            throws StopChildrenCheckLoop {
         final Throwable failure = child.failure.get();
         if (null != failure) {
             logger.warn("Transient thread {} crashed", child.spec.id(), failure);
@@ -159,7 +163,8 @@ public class Supervisor implements Supplier<ChildSpec>, Runnable {
         }
     }
 
-    private void permanentChildStopped(final ThreadConfig child, final AtomicReference<List<Instant>> restarts) {
+    private void permanentChildStopped(final ThreadConfig child, final AtomicReference<List<Instant>> restarts)
+            throws StopChildrenCheckLoop {
         final Throwable failure = child.failure.get();
         if (null != failure) {
             logger.warn("Permanent thread {} crashed", child.spec.id(), failure);
@@ -182,13 +187,16 @@ public class Supervisor implements Supplier<ChildSpec>, Runnable {
         return result;
     }
 
-    private void attemptRestart(final ChildSpec childSpec) {
+    private void attemptRestart(final ChildSpec childSpec) throws StopChildrenCheckLoop {
         switch (flags.strategy()) {
         case ONE_FOR_ONE -> pendingActions.add(new PendingAction(ActionType.RESTART, childSpec));
-        case ONE_FOR_ALL -> children.forEach(threadConfig -> {
-            threadConfig.thread().interrupt();
-            pendingActions.add(new PendingAction(ActionType.RESTART, threadConfig.spec()));
-        });
+        case ONE_FOR_ALL -> {
+            children.forEach(threadConfig -> {
+                threadConfig.thread().interrupt();
+                pendingActions.add(new PendingAction(ActionType.RESTART, threadConfig.spec()));
+            });
+            throw new StopChildrenCheckLoop();
+        }
         case REST_FOR_ONE -> {
             final AtomicBoolean seen = new AtomicBoolean(false);
             children.stream().filter(threadConfig -> {
@@ -198,8 +206,12 @@ public class Supervisor implements Supplier<ChildSpec>, Runnable {
                 threadConfig.thread().interrupt();
                 pendingActions.add(new PendingAction(ActionType.RESTART, threadConfig.spec()));
             });
+            throw new StopChildrenCheckLoop();
         }
         }
+    }
+
+    private class StopChildrenCheckLoop extends Exception {
     }
 
 }
